33d7d2bcefb5a909c9b7d8a9669c0f34f30184ed
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-04-12 12:03:34 -0300

    Configurando URL da API e Configurações Quasar

--- desafio_chuva/src/pages/IndexPage.vue
+++ desafio_chuva/src/pages/IndexPage.vue
@@ -10,7 +10,9 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+// Importando o Axios para receber os valores da API
+import { api } from 'boot/axios'
 
 export default defineComponent({
   name: 'IndexPage',
@@ -19,12 +21,30 @@ export default defineComponent({
     // Nossa Lista do BD
     const posts = ref([])
 
+    // Declarando os Valores referentes a nossa tabela
     const colunas = [
       // Name e Field precisa receber o nome exato da nossa API
       { name: 'id', field: 'id', label: 'ID', align: 'left', sortable: true },
       { name: 'title', field: 'title', label: 'Título', align: 'center', sortable: true },
       { name: 'author', field: 'author', label: 'Autor', align: 'right', sortable: true }
     ]
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    // Função respponsável por chamar os valores do BD
+    // Async -> Função Assincrona
+    // Await -> Função será pausada até que a promise seja resolvida
+    const getPosts = async () => {
+      try {
+        // Caminho da Nossa Aplicação
+        const { data } = await api.get('posts')
+        posts.value = data
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
     return {
       posts,
