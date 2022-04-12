5173aa0098a92a33095f5cce8dc9e9a9a609726d
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-04-12 11:15:11 -0300

    Db.Json e Tabela de Conteúdo
    +1 Exemplo de para o BD
    Configuração de Tabela

--- desafio_chuva/src/pages/IndexPage.vue
+++ desafio_chuva/src/pages/IndexPage.vue
@@ -1,12 +1,35 @@
 <template>
   <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="colunas"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  setup () {
+    // Nossa Lista do BD
+    const posts = ref([])
+
+    const colunas = [
+      // Name e Field precisa receber o nome exato da nossa API
+      { name: 'id', field: 'id', label: 'ID', align: 'left', sortable: true },
+      { name: 'title', field: 'title', label: 'Título', align: 'center', sortable: true },
+      { name: 'author', field: 'author', label: 'Autor', align: 'right', sortable: true }
+    ]
+
+    return {
+      posts,
+      colunas
+    }
+  }
 })
 </script>
