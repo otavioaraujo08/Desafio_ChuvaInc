9ec3ea197617baae3e206e15c2dc14525fb21bb5
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-04-13 19:48:55 -0300

    Página Principal 50%

--- desafio_chuva/src/layouts/MainLayout.vue
+++ desafio_chuva/src/layouts/MainLayout.vue
@@ -1,43 +1,120 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+  <q-layout view="lHh Lpr lFf row">
+    <q-header bordered class="header">
+        <q-toolbar class="justify-around">
+          <div>
+            <span
+              class="header_subtitulo"
+              style="font-size: 14px;"
+            >
+              Anais do Simpósio Latino Americano de Ciências de Alimentos
+            </span>
+            <p
+              class="header_titulo"
+              style="font-size: 22px;"
+            >
+              Anais do 13º Simpósio Latino Americano de Ciência de Alimentos
+            </p>
+            <span
+              class="header_subtitulo"
+              style="
+                font-size: 14px;"
+            >
+              ISSN: 1234-5678
+            </span>
+          </div>
+          <div>
+            <q-select
+              class="seletor"
+              v-model="model"
+              prefix=""
+              square
+              outlined
+            >
+              <template v-slot:prepend>
+                <q-icon name="language" />
+              </template>
+            </q-select>
+          </div>
+          <div>
+            <q-item clickable v-ripple>
+              <q-item-section>
+                <q-item-label class="texto-generico" style="font-size: 20px;">Bem vindo!</q-item-label>
+                <q-item-label class="texto-generico" style="font-size: 15px">alguem12@galoascience.com</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-avatar size="48px">
+                  <img src="https://cdn.quasar.dev/img/avatar.png" />
+                  <q-badge floating color="negative">2</q-badge>
+                </q-avatar>
+              </q-item-section>
+            </q-item>
+          </div>
       </q-toolbar>
     </q-header>
 
+    <!-- Itens dentro da minha sidebar-->
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Menu
-        </q-item-label>
+        v-model="drawer"
+        show-if-above
+        :width="230"
+        :breakpoint="400"
+      >
+        <div style="margin-top: 310px">
+          <q-list padding>
+            <q-item class="topico" clickable v-ripple>
+              <q-item-section class="subtitulos">
+                Apresentação
+              </q-item-section>
+            </q-item>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+            <q-item class="topico" clickable v-ripple>
+              <q-item-section class="subtitulos">
+                Comitês
+              </q-item-section>
+            </q-item>
+
+            <q-item class="topico" clickable v-ripple>
+              <q-item-section class="subtitulos">
+                Autores
+              </q-item-section>
+            </q-item>
+
+            <q-item class="topico" clickable v-ripple>
+              <q-item-section class="subtitulos">
+                Eixos temáticos
+              </q-item-section>
+            </q-item>
+
+            <q-item class="topico_esoclhido" clickable v-ripple>
+              <q-item-section class="subtitulos_escolhido">
+                Trabalhos
+              </q-item-section>
+            </q-item>
+
+            <q-item class="topico" clickable v-ripple>
+              <q-item-section class="subtitulos">
+                Contato
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+
+        <!-- Topo da minha Sidebar -->
+        <q-img
+          class="absolute-top"
+          src="../../public/Logo.png"
+          style="margin-top:50px"/>
+
+        <div
+          class="absolute-top"
+          style="
+            background: linear-gradient(180deg, #FFB354 0%, #EE7A3A 100%);
+            min-height: 75px;"
+        >
+          <p class="titulo">SLACA 2019</p>
+        </div>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,34 +124,92 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Home',
-    caption: '',
-    icon: 'home',
-    link: { name: 'home' }
-  }
-]
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      model: 'PT, BR',
+      leftDrawerOpen
     }
   }
 })
 </script>
+
+<style scoped>
+  /* Retirando a Margem */
+  p, span {
+    margin: 0 0 0 0px;
+  }
+
+  /* Título e Subtítulo do Header */
+  .header_subtitulo, .header_titulo {
+    font-family: 'Roboto';
+    font-style: normal;
+    color: #725C5C;
+  }
+
+  /* Header */
+  .header {
+    background: #FDF1EB;
+    min-height: 60px;
+  }
+
+  /* Componente de select do Quasar */
+  .selector {
+    border: 1px solid #CCCCCC;
+    font-family: 'Quicksand';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    color: #333333;
+  }
+
+  /* Texto utilizado na área de Avatar do Header */
+  .texto-generico {
+    font-style: normal;
+    font-weight: 400;
+    line-height: 18px;
+    text-align: right;
+    color: #333333;
+  }
+
+  /* Título da Sidebar */
+  .titulo {
+    margin: 12px 0px 7px 23px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #FFFFFF;
+  }
+
+  /* Borda na Sidebar */
+  .topico, .topico_esoclhido {
+    border-bottom: 1px solid #E7E7E7;
+  }
+
+  /* Caso seja o tópico esclhido */
+  .topico_esoclhido {
+    background: #FDF1EB;
+    border-left: 5px solid #FAA34D;
+  }
+
+  /* Style dos itens da sidebar */
+  .subtitulos, .subtitulos_escolhido {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 17px;
+    line-height: 19px;
+    color: #725C5C;
+    margin-left: 10px;
+  }
+</style>
--- desafio_chuva/src/pages/IndexPage.vue
+++ desafio_chuva/src/pages/IndexPage.vue
@@ -1,24 +1,146 @@
 <template>
-  <q-page padding>
-    <q-table
-      title="Artigos"
-      :rows="posts"
-      :columns="colunas"
-      row-key="name"
-    >
-      <template v-slot:top>
-        Artigos
+  <div>
+    <!-- Informações do Topo da Tela -->
+    <header class="row justify-between flex-center">
+      <div class="col-8">
+        <h5
+          style="margin-left: 20px; ">
+          Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP
+        </h5>
+      </div>
+
+      <div class="col-auto q-mr-md">
+        <q-btn class="botoes">
+          <img src="../../public/icons/download.png" class="q-mr-md">Download
+        </q-btn>
+        <q-btn class="botoes q-mr-sm q-ml-sm" icon="star"/>
+        <q-btn class="botoes">
+          <img src="../../public/icons/doi.png">
+        </q-btn>
         <q-space/>
-        <q-btn color="primary" label="Adicionar Artigo" :to="{ name: 'formPost' }"/>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td :props="props" class="q-gutter-sm">
-          <q-btn icon="edit" color="info" size="md" dense @click="handlerEditPost(props.row.id)" />
-          <q-btn icon="delete" color="negative" size="md" dense @click="handlerDeletePost(props.row.id)" />
-        </q-td>
-      </template>
-    </q-table>
-  </q-page>
+        <p
+          class="text-center q-mt-md"
+          style="
+            color: #ED7839;
+            font-family: 'Roboto';
+            font-style: normal;
+            font-weight: 500;
+            font-size: 19px;
+            line-height: 25px;"
+        >
+          Como citar esse trabalho ?</p>
+      </div>
+    </header>
+
+    <!-- Informações do Meio da Tela -->
+    <body class="row justify-between flex-center">
+      <div class="col-9 q-pa-md" >
+        <q-video
+          style="width: 100%"
+          :ratio="16/9"
+          src="https://www.youtube.com/embed/WPPPFqsECz0"
+        />
+      </div>
+      <div class="col-auto">
+        <q-card class="my-card" flat bordered >
+          <q-item style="background: #FDF1EB;">
+            <q-item-section >
+              <q-item-label
+                style="
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 500;
+                font-size: 20px;
+                line-height: 25px;
+                color: #4E4E4E;">
+                Detalhes
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-card-section>
+            <q-card-section class="scroll" style="max-width: 260px; max-height: 400px;">
+              <span class="informacoes">Tipo de Apresentação: <b>Pôster</b></span><br>
+              <span class="informacoes">Eixo temático: <b>Alimentação e saúde (AS)</b></span><br>
+              <span class="informacoes">Palavras-chaves: <b> Alimentos funcionais, alimentação escolar.</b></span><br><br>
+              <span class="informacoes"><b>Autores: </b></span><br>
+              <span class="autores">Galileo Galilei¹</span><br>
+              <span class="autores">Berta Lange de Morretes²</span><br>
+              <span class="autores">Isaac Newton³</span><br>
+              <span class="autores">Cesar Lattes¹</span><br>
+              <span class="autores">Stephen Hawking⁴</span><br>
+              <br>
+              <div class="universidades">
+                <span>¹Universidade Estadual de Campinas</span><br>
+                <span>²Universidade de São Paulo</span><br>
+                <span>³Instituto Nacional de Pesquisas Espaciais</span><br>
+                <span>⁴Universidade Federal do Rio de Janeiro</span><br>
+              </div>
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+    </body>
+
+    <article style="margin-left: 20px;">
+       <q-card style="max-width: 98%;" flat bordered>
+          <q-item class="titulo">
+            <q-item-section>
+              <q-item-label
+                style="
+                  font-family: 'Roboto';
+                  color: #4E4E4E;
+                  font-size: 20px;
+                  font-weight: 500;">
+                Resumo
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-card-section horizontal>
+            <q-card-section
+              style="
+              font-family: 'Quicksand';
+              font-style: normal;
+              font-weight: 400;
+              font-size: 14px;
+              line-height: 23px;
+              letter-spacing: 0.02em;
+              color: #5C5C5C;
+              align-self: stretch;">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio. Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa. Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+    </article>
+
+    <div>
+      <q-page padding>
+        <q-table
+          title="Artigos"
+          :rows="posts"
+          :columns="colunas"
+          row-key="name"
+        >
+          <template v-slot:top>
+            Artigos
+            <q-space/>
+            <q-btn color="primary" label="Adicionar Artigo" :to="{ name: 'formPost' }"/>
+          </template>
+          <template v-slot:body-cell-actions="props">
+            <q-td :props="props" class="q-gutter-sm">
+              <q-btn icon="edit" color="info" size="md" dense @click="handlerEditPost(props.row.id)" />
+              <q-btn icon="delete" color="negative" size="md" dense @click="handlerDeletePost(props.row.id)" />
+            </q-td>
+          </template>
+        </q-table>
+      </q-page>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -91,3 +213,67 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+header {
+  display: flex;
+}
+
+ /* Título do Nosso Texto */
+h5 {
+   font-family: 'Roboto';
+   font-style: normal;
+   font-weight: 900;
+   font-size: 20px;
+   line-height: 25px;
+   /* or 125% */
+   letter-spacing: 0.05em;
+   color: #ED7839;
+}
+
+.botoes {
+  margin-top: 30px;
+  font-family: 'Roboto';
+  font-style: normal;
+  background: #ED7839;
+  border-radius: 4px;
+  color: #FFFFFF;
+}
+
+.card-titulo {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 25px;
+  display: flex;
+  align-items: center;
+
+  color: #4E4E4E;
+}
+
+.informacoes, .universidades, .autores {
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%;
+  letter-spacing: 0.02em;
+  color: #5C5C5C;
+}
+
+.informacoes, .autores {
+  font-size: 15px;
+}
+
+.universidades {
+  color: #B7ACAC;
+  font-size: 13px;
+}
+
+.titulo {
+  background: #FDF1EB;
+  border: 1px solid #ECECEC;
+  box-sizing: border-box;
+  border-radius: 3px 3px 0px 0px;
+  font-size: 13px;
+}
+</style>
